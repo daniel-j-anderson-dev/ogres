b2aaa02c681f40989fabebadeb318413de6dd08f
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-12-25 15:46:34 -0700

    Added simple helper function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn get_input(prompt: &str) -> Result<String, std::io::Error> {
+    use std::io::{stdin, stdout, Write};
+
+    stdout().write(prompt.as_bytes())?;
+    
+    let mut input = String::new();
+    stdin().read_line(&mut input)?;
+
+    return Ok(input.trim().to_string());
+}
