66d26174db5491d85d7d82699d7f739950548471
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-12-25 17:19:34 -0700

    Initial steps implementing pseudocode

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,65 @@
+use std::{env, fs};
+use regex::Regex;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = match args.get(1) {
+        Some(file_path) => file_path,
+        None => return Err("Missing file path.".into()),
+    };
+    let regex = match args.get(2) {
+        Some(regex_pattern) => Regex::new(regex_pattern)?,
+        None => return Err("Missing regex pattern.".into()),
+    };
+
+    let file_data = fs::read_to_string(file_path)?;
+
+    for (line_number, line) in file_data.lines().enumerate() {
+        
+        for regex_match in regex.find_iter(line) {
+            let word_index = find_word_index(line, regex_match.start());
+
+            println!(
+                "Found \"{}\" in \"{}\" @ line \"{}\", word \"{}\"",
+                regex,
+                file_path,
+                line_number + 1,
+                word_index,
+            );
+        }
+        
+    }
 
     return Ok(());
 }
 
-fn get_input(prompt: &str) -> Result<String, std::io::Error> {
-    use std::io::{stdin, stdout, Write};
-
-    stdout().write(prompt.as_bytes())?;
-    stdout().flush()?;
-    
-    let mut input = String::new();
-    stdin().read_line(&mut input)?;
-
-    return Ok(input.trim().to_string());
-}
-
 /*
-main
-    args = collect args from environment
-    file_path       = args[1]
-    regex_pattern   = args[2]
-
-    for each line in file
-        if line matches regex
-            print 
-                "Found '{}' in '{}' @ line {}, word {}",
-                regex_pattern,
-                file_path,
-                line_number + 1,
-                find_word_index(line, regex_pattern)
+find_word_index
+    in
+        line: &str
+        word: &str
+    out
+        usize
+    do
+        line
+            .split_whitespace()
+            .position(|w| w == word)
+            .unwrap_or(0) + 1
 */
+
+fn find_word_index(line: &str, start_of_word: usize) -> usize {
+
+    let mut word_index = 0;
+    
+    for (character_index, character) in line.char_indices() {
+        if character.is_whitespace() {
+            word_index += 1;
+        }
+        if character_index == start_of_word {
+            break;
+        }
+    }
+
+    return word_index;
+}
